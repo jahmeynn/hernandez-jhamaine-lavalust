02917c13afdedfd181eb50e71a77e1f5a4348060
jahmeynn <jhamainehernandezll@gmail.com>
2026-08-18 09:51:07 +0800

    first commit

---
+++ app/views/student/home.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My Student Hub</title>
+</head>
+<body>
+
+    <h1>Student Information</h1>
+
+    <p>Welcome to my student page!</p>
+
+    <a href="<?= site_url('student'); ?>">Home</a> |
+    <a href="<?= site_url('student/profile'); ?>">Student Profile</a>
+
+</body>
+</html>
---
+++ app/views/student/profile.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My Student Profile</title>
+</head>
+<body>
+
+    <h1>Student Profile</h1>
+
+    <p>Student ID: YOUR-STUDENT-ID</p>
+    <p>Name: YOUR NAME</p>
+    <p>Course: BS Information Technology</p>
+    <p>Year Level: 2nd Year</p>
+    <p>Section: YOUR SECTION</p>
+    <p>Email: YOUR EMAIL</p>
+
+    <a href="<?= site_url('student'); ?>">Home</a> |
+    <a href="<?= site_url('student/profile'); ?>">Student Profile</a>
+
+</body>
+</html>
